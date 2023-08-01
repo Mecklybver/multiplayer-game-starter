dfc7c556fc2ef83a004259a7999c60f3d5f4c0be
Mecklybver <mecklybver@gmail.com>
2023-08-01 21:14:24 +0200

    update

--- backend.js
+++ backend.js
@@ -1,120 +1,136 @@
-const express = require('express');
-const app = express();
-
-const http = require('http');
-const server = http.createServer(app);
-
-const { Server } = require('socket.io');
-const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 });
-
-const port = 3000;
-
-app.use(express.static('public'));
-
+const express = require('express')
+const app = express()
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+const port = 3000
+app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
-const backendPlayers = {};
-const backendProjectiles = {};
-let projectileId = 0;
+const backEndPlayers = {}
+const backEndProjectiles = {}
+
+const SPEED = 10
+const RADIUS = 10
+const PROJECTILE_RADIUS = 5
+let projectileId = 0
 
 io.on('connection', (socket) => {
-  console.log(`User:${socket.id} connetected.`);
-  backendPlayers[socket.id] = {
+  console.log('a user connected')
+  backEndPlayers[socket.id] = {
     x: 500 * Math.random(),
     y: 500 * Math.random(),
-    color: `hsl(${Math.random() * 360}, 100%, 50%)`,
-  };
+    color: `hsl(${360 * Math.random()}, 100%, 50%)`,
+    sequenceNumber: 0
+  }
 
-  io.emit('updatePlayers', backendPlayers);
+  io.emit('updatePlayers', backEndPlayers)
 
-  socket.on("initCanvas", ({width, height})=>{
-    backendPlayers[socket.id].canvas = {
+  socket.on('initCanvas', ({ width, height, devicePixelRatio }) => {
+    backEndPlayers[socket.id].canvas = {
       width,
       height
     }
 
+    backEndPlayers[socket.id].radius = RADIUS
+
+    if (devicePixelRatio > 1) {
+      backEndPlayers[socket.id].radius = 2 * RADIUS
+    }
   })
 
   socket.on('shoot', ({ x, y, angle }) => {
-    projectileId++;
+    projectileId++
 
     const velocity = {
-      x: Math.cos(angle) * 3,
-      y: Math.sin(angle) * 3,
-    };
+      x: Math.cos(angle) * 5,
+      y: Math.sin(angle) * 5
+    }
 
-    backendProjectiles[projectileId] = {
+    backEndProjectiles[projectileId] = {
       x,
       y,
       velocity,
-      playerId: socket.id,
-    };
-  });
+      playerId: socket.id
+    }
 
-  setInterval(() => {
-    for (const id in backendProjectiles) {
-      backendProjectiles[id].x += backendProjectiles[id].velocity.x;
-      backendProjectiles[id].y += backendProjectiles[id].velocity.y;
-  
-      const radius_projectiles = 5;
+    console.log(backEndProjectiles)
+  })
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason)
+    delete backEndPlayers[socket.id]
+    io.emit('updatePlayers', backEndPlayers)
+  })
+  socket.on('keydown', ({ keycode, sequenceNumber }) => {
+    backEndPlayers[socket.id].sequenceNumber = sequenceNumber
+    switch (keycode) {
+      case 'KeyW':
+        backEndPlayers[socket.id].y -= SPEED
+        break
+      case 'KeyA':
+        backEndPlayers[socket.id].x -= SPEED
+        break
+      case 'KeyS':
+        backEndPlayers[socket.id].y += SPEED
+        break
+      case 'KeyD':
+        backEndPlayers[socket.id].x += SPEED
+        break
+    }
+  })
+
+  console.log(backEndPlayers)
+})
+
+// backend ticker
+setInterval(() => {
+  // update projectile positions
+  for (const id in backEndProjectiles) {
+    backEndProjectiles[id].x += backEndProjectiles[id].velocity.x
+    backEndProjectiles[id].y += backEndProjectiles[id].velocity.y
+
+    const PROJECTILE_RADIUS = 5
+    if (
+      backEndProjectiles[id].x - PROJECTILE_RADIUS >=
+        backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.width ||
+      backEndProjectiles[id].x + PROJECTILE_RADIUS <= 0 ||
+      backEndProjectiles[id].y - PROJECTILE_RADIUS >=
+        backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.height ||
+      backEndProjectiles[id].y + PROJECTILE_RADIUS <= 0
+    ) {
+      delete backEndProjectiles[id]
+      continue
+    }
+
+    for (const playerId in backEndPlayers) {
+      const backEndPlayer = backEndPlayers[playerId]
+
+      const DISTANCE = Math.hypot(
+        backEndProjectiles[id].x - backEndPlayer.x,
+        backEndProjectiles[id].y - backEndPlayer.y
+      )
+
       if (
-        backendProjectiles[id].x - radius_projectiles >=
-          backendPlayers[backendProjectiles[id].playerId]?.canvas?.width ||
-        backendProjectiles[id].x + radius_projectiles <= 0 ||
-        backendProjectiles[id].y - radius_projectiles >=
-          backendPlayers[backendProjectiles[id].playerId]?.canvas?.height ||
-        backendProjectiles[id].y + radius_projectiles <= 0
+        DISTANCE < PROJECTILE_RADIUS + backEndPlayer.radius &&
+        backEndProjectiles[id].playerId !== playerId
       ) {
-        delete backendProjectiles[id];
+        delete backEndProjectiles[id]
+        delete backEndPlayers[playerId]
+        break
       }
     }
-  
-    io.emit('updateProjectiles', backendProjectiles);
-    io.emit('updatePlayers', backendPlayers);
-  }, 15);
-  
+  }
 
-
-  
-  socket.on('disconnect', (reason) => {
-    console.log(reason);
-    delete backendPlayers[socket.id];
-    io.emit('updatePlayers', backendPlayers);
-  });
-
-  const speed = 4
-  
-  socket.on('keydown', (key) => {
-    const player = backendPlayers[socket.id];
-    if (!player) return;
-  
-    switch (key.keycode) {
-      case 'KeyA':
-      case 'ArrowLeft':
-        player.x -= speed;
-        break;
-      case 'KeyD':
-      case 'ArrowRight':
-        player.x += speed;
-        break;
-      case 'KeyW':
-      case 'ArrowUp':
-        player.y -= speed;
-        break;
-      case 'KeyS':
-      case 'ArrowDown':
-        player.y += speed;
-        break;
-      default:
-        break;
-    }
-  });
-  
-
-});
+  io.emit('updateProjectiles', backEndProjectiles)
+  io.emit('updatePlayers', backEndPlayers)
+}, 15)
 
 server.listen(port, () => {
-  console.log(`app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
+console.log('server did load')
